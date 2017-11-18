79470bb81edb7c7ec6830827a86d2172a353ca05
ashah03 <aditshah00@gmail.com>
2017-11-17 17:46:03 -0800

    Fixed the spelling of Independent

--- src/town/Person.java
+++ src/town/Person.java
@@ -8,12 +8,17 @@ public class Person {
 	static int opinions[] = new int[persons.length];
 	public static void main(String[] args) {
 	}
-	public static void genIndependent(){
+	static void genIndependent(){
 			personality = randomGenerator.nextInt(100);
 	}
-	public static void genDependent() {
-		for(int i = 0; i < opinions.length; i++){
-			opinions[i] = (1/personality - persons[i].personality)*100;
+	static void genDependent() {
+		for (int i = 0; i < opinions.length; i++) {
+			opinions[i] = (1 / personality - persons[i].personality) * 100;
 		}
 	}
+	public static void generate(){
+		genIndependent();
+		genDependent();
+	}
 }
+

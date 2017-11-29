9f08af28efc412339603c4536cf56d60dd3e2ab0
ExtraE113 <33768950+ExtraE113@users.noreply.github.com>
2017-11-28 16:10:34 -0800

    Created a gen function and removed static from personalities

--- src/town/Person.java
+++ src/town/Person.java
@@ -5,15 +5,15 @@ import static town.Rand.*; //Allow the use of the Rand class
 import static town.Universe.persons; //Let me access other people
 
 class Person {
-    static int personality; //Make a spot for me to hold my personality
-    static double opinions[] = new double[persons.length]; //Make spots for me to hold my opinions of others
+    int personality; //Make a spot for me to hold my personality
+    double opinions[] = new double[persons.length]; //Make spots for me to hold my opinions of others
 
-    public static void genIndependent() { //create a function to generate independent things
+    public void genIndependent() { //create a function to generate independent things
         personality = randint(100); //Create my personality
         System.out.println("personality generated: " + personality); //Look, user, I made my personality!
     }
 
-    public static void genDependent(int me) { //Create a function to generate dependent
+    public void genDependent(int me) { //Create a function to generate dependent
         for (int i = 0; i < opinions.length; i++) { //Do this for everyone
             if (me != i) {
                 if (personality- persons[i].personality == 0) { //If we're exactly the same
@@ -27,15 +27,7 @@ class Person {
             }
         }
     }
-  /*  public static Person generate(Person person){ //I honestly don't know what this means
-        for(int i = 0; i < persons.length; i++){ //Have every person...
-            person.genIndependent(); //... generate their independent values
-        }
-        for(int i = 0; i < persons.length; i++) { // Have every person...
-            person.genDependent(i); // Generate their dependent values
-        }
-        return person;
-    }*/
+
     void interact(int who, double size, int me) { //Create function for interaction
         if (abs(opinions[who] - persons[who].opinions[me]) >= 40) { //If our opinions are very different set them to be equal
             opinions[who] = persons[who].opinions[me];
--- src/town/Universe.java
+++ src/town/Universe.java
@@ -1,35 +1,40 @@
 package town; //This exists inside package "town"
 
 import java.util.Scanner; //Allow the use of Scanners
+
 import town.Rand; //Allow the use of the Rand class
 import town.Person;
-public class Universe{ //I honestly don't know what this means
-	static Person[] persons; //make a new array of person objects called persons
-	static int numPeople;
-	public static void main(String[] args) { //I honestly don't know what this means
-		Scanner in = new Scanner(System.in); //Create new Scanner
-		//System.out.println("How many people exist in this Universe?"); //Ask the user how many people to make
-		//int numPeople = in.nextInt(); //Get user input
-		numPeople = 20;
-		persons = new Person[numPeople]; //create the requested number of people
-		for(int i = 0; i < numPeople; i++) {
-			persons[i] = new Person();
-			System.out.println(persons[i]);
-			persons[i].genIndependent();
-		}
-		for(int i = 0; i < numPeople; i++) {
-			System.out.println("Person" + i + "s personality is " + persons[i].personality);
-			persons[i].genDependent(i);
-		}
-		System.out.println("For how many epochs would you like this Universe to exist?"); //Ask the user the number of frames
-		int numFrames = in.nextInt(); //set the number of frames as requested
-		for(int i = 0; i<numFrames; i++){ //Run the heart of the simulation
-			int person1 = Rand.randint(numPeople); //pick a random person
-			int person2 = Rand.randint(numPeople); //pick another random person
-			int size = Rand.randint(5); //Decide how big the interaction is
-			persons[person1].interact(person2, size, person1); //Have the people interact
-			persons[person2].interact(person1, size, person2); //Have the people interact
-		}
-	}
+
+public class Universe { //I honestly don't know what this means
+    static Person[] persons; //make a new array of person objects called persons
+    static int numPeople;
+
+    public static void main(String[] args) { //I honestly don't know what this means
+        Scanner in = new Scanner(System.in); //Create new Scanner
+        System.out.println("How many people exist in this Universe?"); //Ask the user how many people to make
+        int numPeople = in.nextInt(); //Get user input
+        persons = new Person[numPeople]; //create the requested number of people
+        gen();
+        System.out.println("For how many epochs would you like this Universe to exist?"); //Ask the user the number of frames
+        int numFrames = in.nextInt(); //set the number of frames as requested
+        for (int i = 0; i < numFrames; i++) { //Run the heart of the simulation
+            int person1 = Rand.randint(numPeople); //pick a random person
+            int person2 = Rand.randint(numPeople); //pick another random person
+            int size = Rand.randint(5); //Decide how big the interaction is
+            persons[person1].interact(person2, size, person1); //Have the people interact
+            persons[person2].interact(person1, size, person2); //Have the people interact
+        }
+    }
+
+    static void gen() {
+        for (int i = 0; i < numPeople; i++) {
+            persons[i] = new Person();
+            persons[i].genIndependent();
+        }
+        for (int i = 0; i < numPeople; i++) {
+            System.out.println("Person" + i + "s personality is " + persons[i].personality);
+            persons[i].genDependent(i);
+        }
+    }
 
 }
